325d57fb3e0c548e15cbd422aba2dbbd395cad5d
tattapan <tattapanwan@ever.app>
2024-03-07 20:54:45 +0700

    feat:seed-data-pattern

---
+++ service/prisma/migrations/20240307132747_add_pattern_data/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Pattern" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "desc" TEXT NOT NULL,
+    "price" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Pattern_pkey" PRIMARY KEY ("id")
+);
--- service/src/modules/pattern/pattern.contrroller.min.ts
+++ service/src/modules/pattern/pattern.contrroller.min.ts
@@ -1,9 +1,13 @@
 import express, { Application, Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 exports.list = async (req: Request, res: Response) => {
   try {
+    const patternData = await prisma.pattern.findMany();
     res.status(200).json({
       code: 200,
       message: "sucess-pattern",
+      data: patternData,
     });
   } catch (error: any) {
     res.status(500).json({
